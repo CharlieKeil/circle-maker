981a89ed97e2ce78df1f82d8776d0dcbfbc220b7
CharlieKeil <Keil0009@algonquinlive.com>
2016-03-15 16:28:39 -0400

    Fixed minor errors.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-var $body = $('html');
+var $html = $('html');
+var $body = $('body');
 
-$body.on('keydown', function (){
+$body.on('keydown', function () {
   var $ball = $('<div>');
+
   $ball.addClass('ball');
   $body.append($ball);
-  $ball.css('left', Math.random()*1400);
-  $ball.css('top', Math.random()*1400);
+  $ball.css('left', Math.random() * 1400);
+  $ball.css('top', Math.random() * 1400);
 });
